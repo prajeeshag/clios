d018c86b2f9f639526c7522313f897a669280e74
prajeesh <prajeeshag@gmail.com>
2024-11-15 03:55:31 +0300

    🚧 wip: work in progress

--- tests/operator/test_OperatorFn.py
+++ tests/operator/test_OperatorFn.py
@@ -62,6 +62,38 @@ def parameters():
 
 
 @pytest.fixture
+def parameters_novar():
+    return (
+        Parameter(
+            name="positional_required",
+            kind=ParameterKind.POSITIONAL_ONLY,
+            param_type=Param(strict=False),
+            annotation=str,
+        ),
+        Parameter(
+            name="positional_optional",
+            kind=ParameterKind.POSITIONAL_ONLY,
+            param_type=Param(strict=False),
+            annotation=str,
+            default="default",
+        ),
+        Parameter(
+            name="kwd_required",
+            kind=ParameterKind.KEYWORD_ONLY,
+            param_type=Param(strict=False),
+            annotation=str,
+        ),
+        Parameter(
+            name="kwd_optional",
+            kind=ParameterKind.KEYWORD_ONLY,
+            param_type=Param(strict=False),
+            annotation=str,
+            default="default",
+        ),
+    )
+
+
+@pytest.fixture
 def return_type():
     return ReturnType(annotation=int)
 
@@ -72,6 +104,16 @@ def operator_fn(parameters, return_type):
         return sum(args)
 
     return OperatorFn(parameters=parameters, return_type=return_type, callback=callback)
+
+
+@pytest.fixture
+def operator_fn_novar(parameters_novar, return_type):
+    def callback(*args, **kwargs):
+        return sum(args)
+
+    return OperatorFn(
+        parameters=parameters_novar, return_type=return_type, callback=callback
+    )
 
 
 def test_positional_params(operator_fn):
@@ -89,10 +131,8 @@ def test_var_positional_param(operator_fn):
 def test_keyword_params(operator_fn):
     keyword_params = operator_fn.kwds
     assert len(keyword_params) == 2
-    assert (keyword_params[0].name, keyword_params[1].name) == (
-        "kwd_required",
-        "kwd_optional",
-    )
+    assert "kwd_required" in keyword_params
+    assert "kwd_optional" in keyword_params
 
 
 def test_var_keyword_param(operator_fn):
@@ -121,4 +161,40 @@ def test_required_positional_params(operator_fn):
 def test_requiered_keyword_params(operator_fn):
     required_keyword_params = operator_fn.required_kwds
     assert len(required_keyword_params) == 1
-    assert required_keyword_params[0].name == "kwd_required"
+    assert "kwd_required" in required_keyword_params
+
+
+def test_iter_args(operator_fn):
+    iter_args = operator_fn.iter_args()
+    arg = next(iter_args)
+    assert arg.name == "positional_required"
+    arg = next(iter_args)
+    assert arg.name == "positional_optional"
+    arg = next(iter_args)
+    assert arg.name == "var_positional"
+    arg = next(iter_args)
+    assert arg.name == "var_positional"
+
+
+def test_iter_args_finite(operator_fn_novar):
+    iter_args = operator_fn_novar.iter_args()
+    arg = next(iter_args)
+    arg.name == "positional_required"
+    arg = next(iter_args)
+    arg.name == "positional_optional"
+    with pytest.raises(StopIteration):
+        arg = next(iter_args)
+
+
+def test_get_kwds(operator_fn):
+    assert operator_fn.get_kwd("kwd_required").name == "kwd_required"
+    assert operator_fn.get_kwd("kwd_optional").name == "kwd_optional"
+    assert operator_fn.get_kwd("not_a_kwarg").name == "var_keyword"
+
+
+def test_get_kwds_finite(operator_fn_novar):
+    operator_fn = operator_fn_novar
+    assert operator_fn.get_kwd("kwd_required").name == "kwd_required"
+    assert operator_fn.get_kwd("kwd_optional").name == "kwd_optional"
+    with pytest.raises(KeyError):
+        operator_fn.get_kwd("not_a_kwarg")
