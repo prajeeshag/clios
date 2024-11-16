00ee462d092ffbf4668424a11d4709845bd81a5c
prajeesh <prajeeshag@gmail.com>
2024-11-16 17:08:54 +0300

    🚧 wip: work in progress

--- examples/calculator/parameters.py
+++ examples/calculator/parameters.py
@@ -1,9 +1,10 @@
 # type: ignore
 parameters = [
-    ["-add 1 2".split(), "3"],
-    ["-sub 1 2".split(), "-1"],
-    ["-add 1 -sub 10 2".split(), "9"],
-    ["-add -add 1 -sub 10 2 100".split(), "109"],
-    ["-sum 1 2 3 4 5".split(), "15"],
-    ["-sub 100 -sum 1 2 3 4 5".split(), "85"],
+    ["-add 1 2".split(), 3],
+    ["-sub 1 2".split(), -1],
+    ["-add 1 -sub 10 2".split(), 9],
+    ["-add -add 1 -sub 10 2 100".split(), 109],
+    ["-sum 1 2 3 4 5".split(), 15],
+    ["-sub 100 -sum 1 2 3 4 5".split(), 85],
+    ["-sub -100 -sum 1 2 3 4 5".split(), -115],
 ]
