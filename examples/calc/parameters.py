ff95af9ef3ffef13d98ba714421d47a5c3a7db64
Prajeesh Ag <prajeeshag@gmail.com>
2024-11-24 17:16:01 +0300

    🚧 wip: work in progress

--- examples/calc/parameters.py
+++ examples/calc/parameters.py
@@ -1,10 +1,7 @@
 # type: ignore
 parameters = [
-    ["-add 1 2".split(), 3],
-    ["-sub 1 2".split(), -1],
-    ["-add 1 -sub 10 2".split(), 9],
-    ["-add -add 1 -sub 10 2 100".split(), 109],
-    ["-sum 1 2 3 4 5".split(), 15],
-    ["-sub 100 -sum 1 2 3 4 5".split(), 85],
-    ["-sub -100 -sum 1 2 3 4 5".split(), -115],
+    ["-print -add 1 2".split(), 3.0],
+    ["-print -sub 1 2".split(), -1.0],
+    ["-print -add 1 -sub 10 2".split(), 9.0],
+    ["-print -add -add 1 -sub 10 2 100".split(), 109.0],
 ]
